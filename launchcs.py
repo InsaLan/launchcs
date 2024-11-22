3b5f17f213000095b1908753377e3f7d855d5034
Awing <awingding.win@gmail.com>
2024-11-22 08:31:34 +0100

    rewritten

--- launchcs.py
+++ launchcs.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
-from os import close
 from subprocess import run as runCommand
-from time import sleep
 import pathlib
+import argparse
 
-SERVER_NUMBER = 17
 INSTANCES_PATH = "/home/cs-servers/msm.d/cs2"
 
-def _init(action: str, numberOfServer: int) -> None:
+def main() -> None:
+    arguments = initializeParser()
+    args = arguments.parse_args()
+    action = args.action
+    numberOfServer = args.numberOfServer
     servers = getServerList()
     runningServers = getRunningServers(servers)
     stoppedServers = [server for server in servers not in runningServers]
@@ -23,9 +25,16 @@ def _init(action: str, numberOfServer: int) -> None:
                 print(f"Can't stop launch this much servers, {len(runningServers)} are running out of the {len(servers)} found")
             for server,_ in zip(stoppedServers, range(numberOfServer)):
                 closeServer(server)
+            print("Stop successfully !")
+        case _:
+            print("Invalid action, please use 'run' or 'stop', here is the help:")
+            arguments.print_help()
 
-
-
+def initializeParser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(prog="Launch CS Servers" ,description="Launch or stop a number of CS2 servers", usage="%(prog)s action numberOfServer")
+    parser.add_argument("action", type=str, help="The action to perform, either 'run' or 'stop'")
+    parser.add_argument("numberOfServer", type=int, help="The number of server to run or stop")
+    return parser
 
 def getServerList() -> list[str]:
     return [f.name[5:] for f in pathlib.Path(INSTANCES_PATH).iterdir() if f.is_dir() and f.name.startswith("inst")]
@@ -39,92 +48,5 @@ def closeServer(server: str) -> None:
 def getRunningServers(serverList: list[str]) -> list[str]:
     return [server for server in serverList if "STOPPED" not in runCommand(f"cs2-server @{server}", shell=True, capture_output=True)]
 
-
-
-def init() -> None:
-    """
-    Try opening the serverlist.txt file to read the list of running servers.
-    Then launch the menu passing it the file content.
-    When exiting the menu, write the list of running servers to the file.
-    """
-    print("Welcome to the CS2 server launcher!")
-    runningServers = []
-    try:
-        runningServersFile = open("serverlist.txt", "r")
-        for line in runningServersFile.readlines():
-            try:
-                num = int(line)
-                runningServers.append(num)
-            except ValueError:
-                print("Invalid server list file. Discarding...")
-    except FileNotFoundError:
-        print("Server list file not found. Discarding...")
-    
-    menu(runningServers)
-    
-    with open("serverlist.txt", "w") as runningServersFile:
-        for server in runningServers:
-            runningServersFile.write(f"{server}\n")
-
-def menu(runningServers: list[int]) -> None:
-    """
-    The main menu, where the user can choose to launch, terminate or exit the program.
-    Args:
-        runningServers (list[int]): The list of running servers.
-    """
-    while True:
-        print("############################")
-        print("# CS2 Server Launcher Menu #")
-        print("############################")
-        print("1. Launch n instances of CS")
-        print("2. Terminate all instances of CS in the server list")
-        print("3. Exit")
-        choice = input("Enter your choice: ")
-        if choice == '1':
-            launch(runningServers)
-            sleep(2)
-        elif choice == '2':
-            terminate(runningServers)
-            sleep(2)
-        elif choice == '3':
-            break
-        else:
-            print("Invalid choice. Try again.")
-
-def launch(runningServers: list[int]) -> None:
-    """
-    Launch n instances of the CS2 server.
-    Args:
-        runningServers (list[int]): The list of running servers.
-    """
-    try:
-        n = int(input(f"Enter the number of instances to launch (available: {SERVER_NUMBER - len(runningServers)}): "))
-    except ValueError:
-        print("Invalid input. Try again.")
-        launch(runningServers)
-        return
-    
-    i = 0
-    while i <= n-1:
-        if i >= SERVER_NUMBER:
-            print("Maximum number of servers reached.")
-            break
-        if i not in runningServers:
-            runCommand(f"cs2-server @server{i} start")
-            runningServers.append(i)
-        else:
-            n+=1
-        i += 1
-
-def terminate(runningServers: list[int]) -> None:
-    """
-    Terminate all instances in the server list file.
-    Args:
-        runningServers (list[int]): The list of running servers.
-    """
-    for server in runningServers:
-        runCommand(f"cs2-server@server{server} stop")
-        runningServers.remove(server)
-
 if __name__ == "__main__":
-    init()
+    main()
