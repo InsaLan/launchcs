3254fdc88ed6d0fdf26605c67cf1cc00cce2c0b9
Awing Ding <awingding.win@gmail.com>
2024-11-22 07:47:53 +0100

    refactor

--- launchcs.py
+++ launchcs.py
@@ -1,8 +1,45 @@
 #!/usr/bin/python
-from os import popen as runCommand
+from os import close
+from subprocess import run as runCommand
 from time import sleep
+import pathlib
 
 SERVER_NUMBER = 17
+INSTANCES_PATH = "/home/cs-servers/msm.d/cs2"
+
+def _init(action: str, numberOfServer: int) -> None:
+    servers = getServerList()
+    runningServers = getRunningServers(servers)
+    stoppedServers = [server for server in servers not in runningServers]
+    match action:
+        case "run":
+            if numberOfServer > len(stoppedServers):
+                print(f"Can't launch this much servers, {len(servers)} are found and {len(runningServers)} are running!")
+            for server,_ in zip(stoppedServers, range(numberOfServer)):
+                launchServer(server)
+            print("Run successfully !")
+        case "stop":
+            if numberOfServer > len(runningServers):
+                print(f"Can't stop launch this much servers, {len(runningServers)} are running out of the {len(servers)} found")
+            for server,_ in zip(stoppedServers, range(numberOfServer)):
+                closeServer(server)
+
+
+
+
+def getServerList() -> list[str]:
+    return [f.name[5:] for f in pathlib.Path(INSTANCES_PATH).iterdir() if f.is_dir() and f.name.startswith("inst")]
+
+def launchServer(server: str) -> None:
+    runCommand(f"cs2-server @{server} start", shell=True)
+
+def closeServer(server: str) -> None:
+    runCommand(f"cs2-server @{server} stop", shell=True)
+
+def getRunningServers(serverList: list[str]) -> list[str]:
+    return [server for server in serverList if "STOPPED" not in runCommand(f"cs2-server @{server}", shell=True, capture_output=True)]
+
+
 
 def init() -> None:
     """
