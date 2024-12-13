35de008a935a7fd7b74fcf4b8b40ef6d82b3d0da
Ecnama <amance@famille-graindorge.fr>
2024-12-13 21:46:52 +0100

    Add "list" action, sort servers

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ Launchcs.py is a python script that launches instances of CS2 multiserver. It is
 python3 launchcs.py start <number of instances>
 
 python3 launchcs.py stop <number of instances>
+
+python3 launchcs.py list
 ```
 
 or
@@ -16,6 +18,8 @@ or
 ./launchcs.py start <number of instances>
 
 ./launchcs.py stop <number of instances>
+
+./launchcs.py list
 ```
 
 ## Requirements
--- launchcs.py
+++ launchcs.py
@@ -18,6 +18,9 @@ def main() -> None:
             if numberOfServer > len(stoppedServers):
                 print(f"Can't launch this much servers, {len(servers)} are found and {len(runningServers)} are running!")
                 return
+            elif numberOfServer == 0:
+                print("Please specify the number of server to start")
+                return
             for server,_ in zip(stoppedServers, range(numberOfServer)):
                 launchServer(server)
             print("Start successfully !")
@@ -25,21 +28,28 @@ def main() -> None:
             if numberOfServer > len(runningServers):
                 print(f"Can't stop launch this much servers, {len(runningServers)} are running out of the {len(servers)} found")
                 return
-            for server,_ in zip(runningServers, range(numberOfServer)):
+            elif numberOfServer == 0:
+                print("Please specify the number of server to stop")
+                return
+            for server,_ in zip(reversed(runningServers), range(numberOfServer)):
                 closeServer(server)
             print("Stop successfully !")
+        case "list":
+            print("Servers found:")
+            for server in servers:
+                print(f" - {server} {'(running)' if server in runningServers else '(stopped)'}")
         case _:
-            print("Invalid action, please use 'run' or 'stop', here is the help:")
+            print("Invalid action, please use 'start' or 'stop', here is the help:")
             arguments.print_help()
 
 def initializeParser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(prog="launchcs" ,description="Launch or stop a number of CS2 servers", usage="%(prog)s action numberOfServer")
-    parser.add_argument("action", type=str, help="The action to perform, either 'start' or 'stop'")
-    parser.add_argument("numberOfServer", type=int, help="The number of server to start or stop")
+    parser.add_argument("action", type=str, choices=["start", "stop", "list"], help="The action to perform, either 'start', 'stop', or 'list'")
+    parser.add_argument("numberOfServer", type=int, nargs='?', default=0, help="The number of server to start or stop")
     return parser
 
 def getServerList() -> list[str]:
-    return [f.name[5:] for f in pathlib.Path(INSTANCES_PATH).iterdir() if f.is_dir() and f.name.startswith("inst")]
+    return [f.name[5:] for f in sorted(pathlib.Path(INSTANCES_PATH).iterdir(), key=lambda f: (len(f.name), f.name)) if f.is_dir() and f.name.startswith("inst")]
 
 def launchServer(server: str) -> None:
     runCommand(f"cs2-server @{server} start", shell=True)
