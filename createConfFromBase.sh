7424ac72ea0a9dac6ad8bcd4d3070d819be178ff
Zagreus <contact@insalan.fr>
2026-03-08 11:01:59 +0100

    fix degeu

--- createConfFromBase.sh
+++ createConfFromBase.sh
@@ -6,13 +6,13 @@
 set -euo
 
 createConfig(){
-	cat base/server.conf | sed -e "s/\(__IP__=\)\"0.0.0.0\"/\1\"172.16.1.15\"/g" -e "s/\(__PORT__=\"\)27015\"/\1$(\awk "BEGIN {x=$1*100; print x+27015}" )\"/g" -e "s/# \(USE_RCON=\"-usercon\"\)/\1/g" -e "s/# \(__RCON_PASS__=\"\)correcthorsebatterystaple\"/\1gamebanana\"/g"	
+	cat base/server.conf | sed -e "s/\(__IP__=\)\"0.0.0.0\"/\1\"172.16.1.13\"/g" -e "s/\(__PORT__=\"\)27015\"/\1$(\awk "BEGIN {x=$1*100; print x+27015}" )\"/g" -e "s/# \(USE_RCON=\"-usercon\"\)/\1/g" -e "s/# \(__RCON_PASS__=\"\)correcthorsebatterystaple\"/\1gamebanana\"/g"
 }
 
-for i in $(seq -w 0 9); do
+for i in $(seq 9); do
 	createConfig $i > "inst-server$i/server.conf"
 done
-for i in $(seq -w 10 17); do
+for i in $(seq -w 10 16); do
 	createConfig $i > "inst-server$i/server.conf"
 done
 
--- plsDontBeDed.py
+++ plsDontBeDed.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from subprocess import run
 from datetime import datetime
+import time
 
 LAUNCHCS_CMD = "/home/cs-servers/launchcs/launchcs.py"
 CS2_SERVER_CMD = "/home/cs-servers/.local/bin/cs2-server"
@@ -32,7 +33,18 @@ def rebootDed():
     servers = getDedServer()
 
     for server in servers:
+        print(f"{CS2_SERVER_CMD} @{server} start")
         process = run(f"{CS2_SERVER_CMD} @{server} start", shell=True, capture_output=True)
+
+        stdout_str = process.stdout.decode("utf-8")
+        stderr_str = process.stderr.decode("utf-8")
+
+        if stderr_str:
+            print(f"ERROR launchcs.py : {stderr_str}")
+        print(stdout_str)
+
         print(f"[{datetime.now()}] - Revived {server}")
 
-rebootDed()
+while 1:
+    rebootDed()
+    time.sleep(10)
